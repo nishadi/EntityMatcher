4597b1de44de78defaac4ab60ce4592e56783884
nishadi <ndimeshi@gmail.com>
2022-05-03 16:57:21 +0530

    Update to persist linkage quality and runtime stats

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
+import csv
+import resource
 import sys
 import getopt
+import time
+
+import fcntl
 import torch
 import deepmatcher as dm
 import deepmatcher.optim as optim
@@ -26,6 +31,7 @@ def run_experiment(model_name, dataset_dir, embedding_dir):
                             embedding_length=300,
                             manualSeed=2)
 
+    start_time = time.time()
     model.run_train(train,
                     validation,
                     epochs=15,
@@ -33,9 +39,47 @@ def run_experiment(model_name, dataset_dir, embedding_dir):
                     label_smoothing=0.05,
                     pos_weight=1.5,
                     best_save_path='best_model_.pth' + gpu_no + '.pth')
+    train_time = time.time() - start_time
+    train_max_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
-    if test is not None:
-        model.run_eval(test)
+    start_time = time.time()
+    stats = model.run_eval(test)
+    test_time = time.time() - start_time
+    test_max_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+    result_file = '/home/remote/u6852937/projects/results.csv'
+    file_exists = os.path.isfile(result_file)
+
+    with open(result_file, 'a') as results_file:
+      heading_list = ['dataset_name', 'train_time', 'test_time',
+                      'train_max_mem', 'test_max_mem', 'TP', 'FP', 'FN',
+                      'TN', 'Pre', 'Re', 'F1', 'Fstar']
+      writer = csv.DictWriter(results_file, fieldnames=heading_list)
+
+      if not file_exists:
+        writer.writeheader()
+
+      p = stats.precision()
+      r = stats.recall()
+      f_star = 0 if (p + r - p * r) == 0 else p * r / (p + r - p * r)
+      fcntl.flock(results_file, fcntl.LOCK_EX)
+      result_dict = {
+        'dataset_name': dataset_dir.split('/')[1],
+        'train_time': round(train_time, 2),
+        'test_time': round(test_time, 2),
+        'train_max_mem': train_max_mem,
+        'test_max_mem': test_max_mem,
+        'TP': stats.tps,
+        'FP': stats.fps,
+        'FN': stats.fns,
+        'TN': stats.tns,
+        'Pre': round(p * 100, 2),
+        'Re': round(r * 100, 2),
+        'F1': round(stats.f1() * 100, 2),
+        'Fstar': round(f_star * 100, 2)
+      }
+      writer.writerow(result_dict)
+      fcntl.flock(results_file, fcntl.LOCK_UN)
 
 
 def get_params(argv):
