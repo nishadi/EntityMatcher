f8f53abd543c85ae653eadb84fb55c1ada51cb40
nishadi <ndimeshi@gmail.com>
2022-05-20 18:40:23 +0530

    Fix the issue in retrieving stats

--- run.py
+++ run.py
@@ -43,7 +43,7 @@ def run_experiment(model_name, dataset_dir, embedding_dir):
     train_max_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
     start_time = time.time()
-    stats = model.run_eval(test)
+    stats = model.run_eval(test, return_stats=True)
     test_time = time.time() - start_time
     test_max_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
@@ -51,7 +51,7 @@ def run_experiment(model_name, dataset_dir, embedding_dir):
     file_exists = os.path.isfile(result_file)
 
     with open(result_file, 'a') as results_file:
-      heading_list = ['dataset_name', 'train_time', 'test_time',
+      heading_list = ['method', 'dataset_name', 'train_time', 'test_time',
                       'train_max_mem', 'test_max_mem', 'TP', 'FP', 'FN',
                       'TN', 'Pre', 'Re', 'F1', 'Fstar']
       writer = csv.DictWriter(results_file, fieldnames=heading_list)
@@ -59,24 +59,25 @@ def run_experiment(model_name, dataset_dir, embedding_dir):
       if not file_exists:
         writer.writeheader()
 
-      p = stats.precision()
-      r = stats.recall()
+      p = stats.precision() / 100.0
+      r = stats.recall() / 100.0
       f_star = 0 if (p + r - p * r) == 0 else p * r / (p + r - p * r)
       fcntl.flock(results_file, fcntl.LOCK_EX)
       result_dict = {
+        'method' : 'hiermatcher',
         'dataset_name': dataset_dir.split('/')[1],
         'train_time': round(train_time, 2),
         'test_time': round(test_time, 2),
         'train_max_mem': train_max_mem,
         'test_max_mem': test_max_mem,
-        'TP': stats.tps,
-        'FP': stats.fps,
-        'FN': stats.fns,
-        'TN': stats.tns,
-        'Pre': round(p * 100, 2),
-        'Re': round(r * 100, 2),
-        'F1': round(stats.f1() * 100, 2),
-        'Fstar': round(f_star * 100, 2)
+        'TP': stats.tps.item(),
+        'FP': stats.fps.item(),
+        'FN': stats.fns.item(),
+        'TN': stats.tns.item(),
+        'Pre': ('{prec:.2f}').format(prec = stats.precision()),
+        'Re': ('{rec:.2f}').format(rec = stats.recall()),
+        'F1': ('{f1:.2f}').format(f1 = stats.f1()),
+        'Fstar': ('{fstar:.2f}').format(fstar = f_star * 100)
       }
       writer.writerow(result_dict)
       fcntl.flock(results_file, fcntl.LOCK_UN)
